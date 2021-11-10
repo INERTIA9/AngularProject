c19e28c14e0d67e51ee861a8c9d05097b35512b1
saurabh <dekapurwars@gmail.com>
2021-11-10 19:55:55 +0530

    Added: path for ourworlds,experiences and gallery component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,21 @@ import { FooterComponent } from './Components/footer/footer.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { HomepageComponent } from './Components/homepage/homepage.component';
 import { AboutdlcComponent } from './Components/aboutdlc/aboutdlc.component';
+import { OurworldsComponent } from './Components/ourworlds/ourworlds.component';
+import { ExperiencesComponent } from './Components/experiences/experiences.component';
+import { GalleryComponent } from './Components/gallery/gallery.component';
 
 
 
 const routes: Routes = [
-  {path:'header', component: HeaderComponent},
-  {path:'footer', component: FooterComponent},
-  {path:'homepage', component:HomepageComponent},
-  { path:'aboutdlc', component:AboutdlcComponent},
-  
+  { path: 'header', component: HeaderComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: 'homepage', component: HomepageComponent },
+  { path: 'aboutdlc', component: AboutdlcComponent },
+  { path: 'ourworlds', component: OurworldsComponent },
+  { path: 'experiences', component: ExperiencesComponent },
+  { path: 'gallery', component: GalleryComponent },
+
 
 ];
 
